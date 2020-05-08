91cff9a446fa1b89832fc0607972aec7c0f5e85c
Hey-Mikelangelo <spikeewave@gmail.com>
2020-05-08 21:03:44 +0300

    added comments

--- BoardClass.h
+++ BoardClass.h
@@ -10,6 +10,7 @@ private:
     int queensCount;
 public:
     Board(int width, int queensNeeded) : width(width), queensNeeded(queensNeeded) {
+        //make all cells zeros;
         InitCells();
     }
     ~Board() {
@@ -37,33 +38,50 @@ public:
 
 
 char** Board::TrySetQueen() {
+    //if it is impossible to place Queens on table 
     if (queensNeeded > width) {
+        // - end before the mess starts;
         return nullptr;
     }
+    //if last needed funtion call called recursion once more. 
+    //("if" statement with ID = LastCall. You'll understand later)
     if (queensCount >= queensNeeded) {
+        // -soltution found
         return cells;
     }
+    //the mess starts
     for (int i = 0; i < width; i++) {
         for (int j = 0; j < width; j++) {
+            //if current cell is free (0 == false)
             if (!cells[i][j]) {
+                //place Queen and mark cells which are no more safe to stay on
                 PlaceQueen(i, j);
+                //Trying to place another Queen. ("if" with ID = QueenRemoverIf)
                 if (TrySetQueen() == nullptr) {
+                    //if free position not found - remove placed earlier Queen
                     RemoveQueen(i, j);
                 }
+                //if program execution reached this place, it means that "if" with ID = LastCall returned not nullptr
                 else {
+                    //returning cells array. 
+                    //From this place from stack finally shrinks.
                     return cells;
                 }
             }
         }
     }
+    //if tried all possible cells and not found a free one, return nullptr.
+    //"if" with ID = QueenRemoverIf will accept this result and remove Queen which caused this situation.
     return nullptr;
 }
 
 void Board::PlaceQueen(const int row, const int col) {
+    //nothing special.
     int foo;
     for (int i = 0; i < width; i++) {
         ++cells[row][i];
         ++cells[i][col];
+        //you do not need to understand thus
         foo = col - row + i;
         if (foo >= 0 && foo < width) {
             ++cells[i][foo];
@@ -79,10 +97,12 @@ void Board::PlaceQueen(const int row, const int col) {
 
 
 void Board::RemoveQueen(const int row, const int col) {
+    //nothing special.
     int foo;
     for (int i = 0; i < width; i++) {
         --cells[row][i];
         --cells[i][col];
+        //you do not need to understand this
         foo = col - row + i;
         if (foo >= 0 && foo < width) {
             --cells[i][foo];
@@ -97,18 +117,19 @@ void Board::RemoveQueen(const int row, const int col) {
 }
 
 void Board::DrawBoard() {
+    //simple std::cout
     for (int i = 0; i < width; i++) {
         for (int j = 0; j < width; j++) {
             if (cells[i][j] == 0) {
-                cout << ". ";
+                cout<< ". ";
             }
             else if (cells[i][j] > 0) {
-                cout << "x ";
+                cout<< "x ";
             }
             else if (cells[i][j] == -1) {
-                cout << "O ";
+                cout<< "O ";
             }
         }
-        cout << endl;
+        cout<< endl;
     }
 }
